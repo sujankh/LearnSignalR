01aada3f68b4de4b16f4ecfa9881ecb416cbbe31
sujankh <kcsujan@gmail.com>
2014-03-22 14:09:13 +0545

    Server loop added

--- MoveShape/MoveShapeHub.cs
+++ MoveShape/MoveShapeHub.cs
@@ -1,14 +1,66 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System;
+using System.Threading;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 using Newtonsoft.Json;
 
 namespace MoveShape
 {
+    public class ShapeBroadCaster
+    {
+        private static Lazy<ShapeBroadCaster> singleton = 
+            new Lazy<ShapeBroadCaster>(() => new ShapeBroadCaster());
+
+        private readonly TimeSpan BroadCastInterval = TimeSpan.FromMilliseconds(40);
+
+        private bool shouldUpdateShape;
+
+        private IHubContext shapeHub;
+        private ShapeModel shapeModel;
+
+        public ShapeBroadCaster()
+        {
+            var broadCastLoop = new Timer(UpdateShape, null, BroadCastInterval, BroadCastInterval);
+            shouldUpdateShape = false;
+
+            shapeHub = GlobalHost.ConnectionManager.GetHubContext<MoveShapeHub>();
+        }
+
+        private void UpdateShape(object state)
+        {
+            if (shouldUpdateShape)
+            {
+                shapeHub.Clients.AllExcept(shapeModel.LastUpdatedBy).updateShape(shapeModel);
+
+                shouldUpdateShape = false;
+            }
+        }
+
+        public void UpdateModel(ShapeModel shapeModel)
+        {
+            this.shapeModel = shapeModel;
+            shouldUpdateShape = true;
+            UpdateShape(null);
+        }
+
+        public static ShapeBroadCaster Instance
+        {
+            get { return singleton.Value; }
+        }
+
+    }
+
     public class MoveShapeHub : Hub
     {
+        private ShapeBroadCaster shapeBroadCaster;
+        public MoveShapeHub()
+        {
+            this.shapeBroadCaster = ShapeBroadCaster.Instance;
+        }
         public void UpdateModel(ShapeModel clientModel)
         {
-            clientModel.LastUpdatedBy = Context.ConnectionId;
-            Clients.AllExcept(clientModel.LastUpdatedBy).updateShape(clientModel);
+            clientModel.LastUpdatedBy = Context.ConnectionId;            
+            shapeBroadCaster.UpdateModel(clientModel);
         }
     }
 
