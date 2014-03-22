f72b27a063cadcf3378aceab965271545c642eb8
sujankh <kcsujan@gmail.com>
2014-03-22 15:11:20 +0545

    Errors in project fixed

--- StockTicker/StockTickerHub.cs
+++ StockTicker/StockTickerHub.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Microsoft.AspNet.SignalR;
+﻿using Microsoft.AspNet.SignalR;
 
 namespace StockTicker
 {
-    public class StockTicker
-    {
-
-    }
-   
     public class StockTickerHub : Hub
     {
         public void Hello()
