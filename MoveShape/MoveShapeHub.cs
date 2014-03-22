b7297b3f817e1b7c922c4693593a75a9cef254cd
sujankh <kcsujan@gmail.com>
2014-03-22 11:57:49 +0545

    MoveShape updated

---
+++ MoveShape/MoveShapeHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+
+namespace MoveShape
+{
+    public class MoveShapeHub : Hub
+    {
+        public void UpdateModel(ShapeModel clientModel)
+        {
+            clientModel.LastUpdatedBy = Context.ConnectionId;
+            Clients.AllExcept(clientModel.LastUpdatedBy).updateShape(clientModel);
+        }
+    }
+
+    public class ShapeModel
+    {
+        [JsonProperty("left")]
+        public double Left { get; set; }
+
+        [JsonProperty("top")]
+        public double Top { get; set; }
+
+        [JsonIgnore]
+        public string LastUpdatedBy { get; set; }
+
+    }
+}
